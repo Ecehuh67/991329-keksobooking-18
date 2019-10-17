1ef3f67dc0b6b42ec126ca35b0904bf7895478e2
Гендольф <ecehuh67@gmail.com>
2019-10-17 12:28:54 +0300

    do not know

--- js/filter.js
+++ js/filter.js
@@ -3,26 +3,68 @@
 (function () {
   var typeOfAccomodation;
   var pins = [];
+  var temporary = [];
+  var price;
+  var rooms;
+  var prices = {
+    'any': 0,
+    'middle': {
+      min: 10000,
+      max: 50000
+    },
+    'low': 10000,
+    'high': 50000
+  };
+
+
+  var getRangePrice = function (cost) {
+    var typeOfPrice;
+     if (cost < prices.low ) {
+      typeOfPrice = 'low';
+    } else if (cost > prices.middle.min && cost < prices.middle.max) {
+      typeOfPrice = 'middle';
+    } else if (cost > prices.high) {
+      typeOfPrice = 'high';
+    }
+    return typeOfPrice;
+  };
 
   var updatePins = function () {
-    if (typeOfAccomodation === 'any') {
-      window.render.render(pins);
-    } else {
-      window.render.render(pins.slice().filter(function (pin) {
-        return pin.offer.type === typeOfAccomodation;
-      }));
-    }
+    //console.log(temporary);
+    window.render.render(temporary);
   };
 
   window.pin.onAccomodationChange = function (type) {
-    typeOfAccomodation = type;
+    if (type === 'any') {
+      temporary = pins;
+    } else {
+      temporary = pins.filter(function (pin) {
+        return pin.offer.type === type;
+      });
+    }
+    updatePins();
+  };
+
+  // window.pin.onPriceChange = function (amount) {
+  //
+  //   updatePins();
+  // };
+
+  window.pin.onRoomsChange = function (room) {
+    if (room === 'any') {
+      temporary = pins;
+    } else {
+      temporary = pins.filter(function (pin) {
+        return pin.offer.rooms == room;
+      });
+    }
     updatePins();
   };
 
   // Callback for rendering pins from server data
   var successHandler = function (data) {
     pins = data;
-    window.render.render(data);
+    temporary = pins;
 
     // Создаем буфер куда будем временно копировать объявления
     var advert = document.createDocumentFragment();
@@ -34,7 +76,6 @@
     window.render.map.insertBefore(advert, window.render.map.querySelector('.map__filters-container'));
 
   };
-
 
   // Callback for showing a error message if data isn't loaded from server
   var errorHandler = function () {
--- js/pin.js
+++ js/pin.js
@@ -2,13 +2,38 @@
 
 (function () {
   var pin = {
-    onAccomodationChange: function () {}
+    onAccomodationChange: function () {},
+    onPriceChange: function () {},
+    onRoomsChange: function () {},
+    onGuestsChange: function () {},
+    onFeaturesChange: function () {}
   };
 
   var typeOfHouse = document.querySelector('#housing-type');
+  var price = document.querySelector('#housing-price');
+  var rooms = document.querySelector('#housing-rooms');
+  var guests = document.querySelector('#housing-guests');
+  var features = document.querySelector('#housing-features');
+
   typeOfHouse.addEventListener('change', function (evt) {
     var value = evt.target.value;
     pin.onAccomodationChange(value);
+  });
+  price.addEventListener('change', function (evt) {
+    var value = evt.target.value;
+    pin.onPriceChange(value);
+  });
+  rooms.addEventListener('change', function (evt) {
+    var value = evt.target.value;
+    pin.onRoomsChange(value);
+  });
+  guests.addEventListener('change', function (evt) {
+    var value = evt.target.value;
+    pin.onGuestsChange(value);
+  });
+  features.addEventListener('change', function (evt) {
+    var value = evt.target.value;
+    pin.onFeaturesChange(value);
   });
 
   window.pin = pin;
